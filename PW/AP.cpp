ace8fcfca3b6c76cf1e089430984a30d8b0200b5
pranalidhole13 <pranalidhole63@gmail.com>
2024-02-23 12:53:51 +0530

    commit

--- PW/AP.cpp
+++ PW/AP.cpp
@@ -8,8 +8,16 @@ int main()
     int n;
     cout << "Enter number: ";
     cin >> n;
-    for (int i = 1; i <= 2 * n - 1; i += 2)
+    // for (int i = 1; i <= 2 * n - 1; i += 2)
+    // {
+    //     cout << i << endl;
+    // }
+
+    // 4 7 10 13 16 19
+    int a = 4;
+    for (int i = 1; i <= n; i++)
     {
-        cout << i << endl;
+        cout << a << endl;
+        a = a + 3;
     }
 }
