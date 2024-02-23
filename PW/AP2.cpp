a592c41656b387637f418d93d5fd2449d337448e
pranalidhole13 <pranalidhole63@gmail.com>
2024-02-23 20:08:20 +0530

    commit

---
+++ PW/AP2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 10;
+    int a = 100;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << a << endl;
+        a = a - 3;
+    }
+}
