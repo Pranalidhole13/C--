78821a6e08fb7a67da6887f9453b16913d2e78fa
pranalidhole13 <pranalidhole63@gmail.com>
2024-02-25 23:32:59 +0530

    commit

---
+++ PW/Loop1/table.cpp
@@ -0,0 +1,13 @@
+// Q3. Print the table of ‘n’. Here ‘n’ is an integer which the user will input.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << n * i << endl;
+    }
+}
